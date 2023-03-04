63087361dde53c61ca3bb3b2d8549ebc7860c528
Dmitriy Averyanov <dimthomas1989@gmail.com>
2023-03-04 14:28:47 +0300

    Get data drom internet

--- app/src/main/java/com/dimthomas/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/dimthomas/cryptoapp/MainActivity.kt
@@ -2,9 +2,16 @@ package com.dimthomas.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.dimthomas.cryptoapp.api.ApiFactory
 import com.dimthomas.cryptoapp.databinding.ActivityMainBinding
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,5 +20,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.textHelloWorld.text = "Test binding"
+        val disposable =  ApiFactory.apiService.getFullPriceList(fSyms = "BTC,ETH,EOS")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            },{
+                it.message?.let { it1 -> Log.d("TEST_OF_LOADING_DATA", it1) }
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
