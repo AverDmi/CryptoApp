f12e956c15f4ce5ff3a02ba168fccaabab8e572e
Dmitriy Averyanov <dimthomas1989@gmail.com>
2023-07-04 21:41:45 +0300

    Refactoring. Add view binding to adapter

--- app/src/main/java/com/dimthomas/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
+++ app/src/main/java/com/dimthomas/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
@@ -5,13 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.dimthomas.cryptoapp.R
+import com.dimthomas.cryptoapp.databinding.ItemCoinInfoBinding
 import com.dimthomas.cryptoapp.domain.CoinInfo
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.item_coin_info.view.*
 
-class CoinInfoAdapter(private val context: Context): Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
+class CoinInfoAdapter(private val context: Context): Adapter<CoinInfoViewHolder>() {
 
     var coinInfoList: List<CoinInfo> = listOf()
     set(value) {
@@ -21,22 +20,18 @@ class CoinInfoAdapter(private val context: Context): Adapter<CoinInfoAdapter.Coi
 
     var onCoinClickListener: OnCoinClickListener? = null
 
-
-    inner class CoinInfoViewHolder(itemView: View): ViewHolder(itemView) {
-        val ivLogoCoin = itemView.ivLogoCoin
-        val tvSymbols = itemView.tvSymbols
-        val tvPrice = itemView.tvPrice
-        val tvLastUpdate = itemView.tvLastUpdate
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_coin_info, parent, false)
-        return CoinInfoViewHolder(view)
+        val binding = ItemCoinInfoBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return CoinInfoViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
         val coin = coinInfoList[position]
-        with(holder) {
+        with(holder.binding) {
             with(coin) {
                 val symbolsTemplate = context.resources.getString(R.string.symbols_template)
                 val lastUpdateTemplate = context.resources.getString(R.string.last_update_template)
@@ -44,7 +39,7 @@ class CoinInfoAdapter(private val context: Context): Adapter<CoinInfoAdapter.Coi
                 tvPrice.text = price
                 tvLastUpdate.text = String.format(lastUpdateTemplate, lastUpdate)
                 Picasso.get().load(imageUrl).into(ivLogoCoin)
-                itemView.setOnClickListener {
+                root.setOnClickListener {
                     onCoinClickListener?.onCoinClick(this)
                 }
             }
---
+++ app/src/main/java/com/dimthomas/cryptoapp/presentation/adapters/CoinInfoViewHolder.kt
@@ -0,0 +1,8 @@
+package com.dimthomas.cryptoapp.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.dimthomas.cryptoapp.databinding.ItemCoinInfoBinding
+
+class CoinInfoViewHolder(
+    val binding: ItemCoinInfoBinding
+): RecyclerView.ViewHolder(binding.root)
